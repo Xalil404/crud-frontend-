9e27bc98bb2f41c55208db1c4668de9acf8dad45
Kareem <kareemkv1990@yahoo.com>
2024-10-30 11:55:39 +0000

    Added google integration to login page

--- crud-frontend/src/components/auth/Login.js
+++ crud-frontend/src/components/auth/Login.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { loginUser } from '../../services/api';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -29,101 +30,137 @@ const Login = () => {
         }
     };
 
+    const responseGoogle = async (response) => {
+        console.log("Google Response:", response);
+
+        // Construct user data with token
+        const userData = {
+            token: response.credential,
+        };
+
+        // Send token to your Django backend
+        try {
+            const res = await fetch('https://crud-backend-for-react-841cbc3a6949.herokuapp.com/api/auth/google/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userData),
+            });
+            const data = await res.json();
+            console.log("Backend Response:", data);
+
+            if (data.token) {
+                // Save the token locally and redirect
+                localStorage.setItem('authToken', data.token);
+                navigate('/dashboard'); // Redirect after successful login
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            setError('Google login failed. Please try again.');
+        }
+    };
+
     return (
-        <div className="container mt-5">
-            <div className="row">
-                {/* Left Column for Image */}
-                <div className="col-md-6 d-none d-md-block">
-                    <img
-                        src="https://res.cloudinary.com/dnbbm9vzi/image/upload/v1729335598/flag-icon_1_qrosti.png"
-                        className="img-fluid"
-                        alt="Login"
-                    />
-                </div>
+        <GoogleOAuthProvider clientId="17966200055-rc9cu06jutt8mo0cqob43vhnejvbltd4.apps.googleusercontent.com">
+            <div className="container mt-5">
+                <div className="row">
+                    {/* Left Column for Image */}
+                    <div className="col-md-6 d-none d-md-block">
+                        <img
+                            src="https://res.cloudinary.com/dnbbm9vzi/image/upload/v1729335598/flag-icon_1_qrosti.png"
+                            className="img-fluid"
+                            alt="Login"
+                        />
+                    </div>
 
-                {/* Right Column for Login Form */}
-                <div className="col-md-6">
-                    <div className="card" style={{ border: 'none' }}>
-                        <div className="card-header-a text-center card-header-custom">
-                            <h2>Sign in to Dates</h2>
-                        </div>
-
-                        <div className="card-body">
-                            {/* Social login */}
-                            <div className="social-login-buttons text-center mb-5">
-                                <button
-                                    type="button"
-                                    className="btn btn-danger mb-4 w-100 py-3 mx-auto d-block rounded-button"
-                                    onClick={() => {
-                                        window.location.href = 'https://crud-backend-for-react-841cbc3a6949.herokuapp.com/accounts/google/login/';
-                                      }}
-                                >
-                                    <i className="fab fa-google"></i> Login with Google
-                                </button>
+                    {/* Right Column for Login Form */}
+                    <div className="col-md-6">
+                        <div className="card" style={{ border: 'none' }}>
+                            <div className="card-header-a text-center card-header-custom">
+                                <h2>Sign in to Dates</h2>
                             </div>
 
-                            {/* Divider with text */}
-                            <div className="text-center mb-5">
-                                <div className="divider-login">
-                                    <hr className="divider-line" />
-                                    <span className="divider-text mx-2">or sign in with email</span>
-                                    <hr className="divider-line" />
-                                </div>
-                            </div>
-
-
-                            {/* Login Form */}
-                            <form onSubmit={handleSubmit}>
-                                <input
-                                    type="email"
-                                    className="form-control mb-3"
-                                    placeholder="Email"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    required
-                                />
-                                <input
-                                    type="password"
-                                    className="form-control mb-3"
-                                    placeholder="Password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    required
-                                />
-                                <div className="social-login-buttons">
-                                    <button
-                                        type="submit"
-                                        className="btn btn-lg mx-auto d-block w-100 py-3 rounded-button"
-                                        style={{ backgroundColor: '#E8BF73', color: 'black' }}
-                                        disabled={loading}
+                            <div className="card-body">
+                                {/* Social login */}
+                                <div className="social-login-buttons text-center mb-5">
+                                    <GoogleLogin
+                                        onSuccess={responseGoogle}
+                                        onError={() => console.error('Login Failed')}
+                                        ux_mode="popup"
+                                        cookiePolicy="single_host_origin"
                                     >
-                                        {loading ? 'Logging in...' : 'Sign In'}
-                                    </button>
+                                        <button
+                                            type="button"
+                                            className="btn btn-danger mb-4 w-100 py-3 mx-auto d-block rounded-button"
+                                        >
+                                            <i className="fab fa-google"></i> Login with Google
+                                        </button>
+                                    </GoogleLogin>
                                 </div>
-                            </form>
 
-                            {/* Error Message */}
-                            {error && <p style={{ color: 'red' }}>{error}</p>}
+                                {/* Divider with text */}
+                                <div className="text-center mb-5">
+                                    <div className="divider-login">
+                                        <hr className="divider-line" />
+                                        <span className="divider-text mx-2">or sign in with email</span>
+                                        <hr className="divider-line" />
+                                    </div>
+                                </div>
 
-                            <hr />
+                                {/* Login Form */}
+                                <form onSubmit={handleSubmit}>
+                                    <input
+                                        type="email"
+                                        className="form-control mb-3"
+                                        placeholder="Email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        required
+                                    />
+                                    <input
+                                        type="password"
+                                        className="form-control mb-3"
+                                        placeholder="Password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                    />
+                                    <div className="social-login-buttons">
+                                        <button
+                                            type="submit"
+                                            className="btn btn-lg mx-auto d-block w-100 py-3 rounded-button"
+                                            style={{ backgroundColor: '#E8BF73', color: 'black' }}
+                                            disabled={loading}
+                                        >
+                                            {loading ? 'Logging in...' : 'Sign In'}
+                                        </button>
+                                    </div>
+                                </form>
 
-                            {/* Links for Forgot Password and Sign Up */}
-                            <p className="text-center">
-                                <a href="/reset-password" className="text-dark">
-                                    Forgot Password?
-                                </a>
-                            </p>
-                            <p className="text-center">
-                                Don't have an account?{' '}
-                                <a href="/register" className="text-dark">
-                                    Sign up
-                                </a>
-                            </p>
+                                {/* Error Message */}
+                                {error && <p style={{ color: 'red' }}>{error}</p>}
+
+                                <hr />
+
+                                {/* Links for Forgot Password and Sign Up */}
+                                <p className="text-center">
+                                    <a href="/reset-password" className="text-dark">
+                                        Forgot Password?
+                                    </a>
+                                </p>
+                                <p className="text-center">
+                                    Don't have an account?{' '}
+                                    <a href="/register" className="text-dark">
+                                        Sign up
+                                    </a>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </GoogleOAuthProvider>
     );
 };
 
